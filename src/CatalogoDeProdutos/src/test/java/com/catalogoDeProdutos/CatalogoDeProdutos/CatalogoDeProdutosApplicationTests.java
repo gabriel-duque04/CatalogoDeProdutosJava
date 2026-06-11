dbdff4df03de1a46041f5319ec113adc2e0b8049
Gabriel <gabrielduquefernandes@gmail.com>
2026-06-11 10:49:49 -0300

    Commit inicial do sistema

---
+++ src/CatalogoDeProdutos/src/test/java/com/catalogoDeProdutos/CatalogoDeProdutos/CatalogoDeProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalogoDeProdutos.CatalogoDeProdutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoDeProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
