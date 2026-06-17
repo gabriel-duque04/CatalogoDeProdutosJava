7d1ef56e540b5c341b42fbe1747932e48823f20b
Gabriel <gabrielduquefernandes@gmail.com>
2026-06-17 11:17:09 -0300

    Implementação da entidade categoria

--- src/CatalogoDeProdutos/src/main/java/Domain/Entities/Categoria.java
+++ src/CatalogoDeProdutos/src/main/java/Domain/Entities/Categoria.java
@@ -1,4 +1,32 @@
 package Domain.Entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "Categoria")
 public class Categoria {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column(name = "Nome")
+    private String nome;
+
+    @Column(name = "Descricao")
+    private String descricao;
+
+    @Column(name  = "DataDeCriacao")
+    private Timestamp dataDeCriacao;
+
 }
