32afbe95d1bb034ac5478c2e2c0878ae583e039e
Gabriel <gabrielduquefernandes@gmail.com>
2026-06-17 11:29:51 -0300

    Implementação da entidade produtos

--- src/CatalogoDeProdutos/src/main/java/Domain/Entities/Produto.java
+++ src/CatalogoDeProdutos/src/main/java/Domain/Entities/Produto.java
@@ -1,4 +1,39 @@
 package Domain.Entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "Produto")
 public class Produto {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column(name = "Nome")
+    private String nome;
+
+    @Column(name = "Descricao")
+    private String descricao;
+
+    @Column(name = "Preco")
+    private double preco;
+
+    @Column(name = "CategoriaId")
+    private int categoriaId;
+
+    @Column(name = "Ativo")
+    private boolean ativo;
+
+    @Column(name = "DataDeCriacao")
+    private Timestamp dataDeCriacao;
 }
